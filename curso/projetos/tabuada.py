2a677ebd3486fafab3f47c95fbe8e7258fa8c1ef
jotape0712 <bezerrajoaopedro225@gmail.com>
2025-03-26 21:21:42 -0300

    envio

--- curso/projetos/tabuada.py
+++ curso/projetos/tabuada.py
@@ -1,5 +1,8 @@
-numero = int(input("Escolha um numero para ver a tabuada: "))
-tabuada = 0
 
-for tabuada in range(0,11):
-    print(f"{numero} x {tabuada} = {numero * tabuada}")
+numero = int(input("Escolha um numero para ver a tabuada: "))  
+tabuada = 0  
+
+# Cria um loop que vai de 0 até 10 (o range inclui o 0 e o 10, perfeito para a tabuada)
+for tabuada in range(0, 11):  
+    # Exibe a operação (multiplicação) no formato: numero x tabuada = resultado
+    print(f"{numero} x {tabuada} = {numero * tabuada}")  
