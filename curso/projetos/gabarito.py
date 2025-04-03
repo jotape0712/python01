ea9d847bfc768deb7e2a17771e4af8efc6de2309
Joao Pedro Bezerra <joaopedrobezerradev@gmail.com>
2025-04-03 14:33:14 -0300

    ...

--- curso/projetos/gabarito.py
+++ curso/projetos/gabarito.py
@@ -1,17 +1,16 @@
 
-gabarito = ["A", "B", "B", "D", "C", "A", "D", "C", "A"]
+gabarito = ["A", "B", "B", "D", "C", "A", "D", "C", "A"] # Lista contendo o gabarito correto das questões
 
-respostas = ["B", "C", "B", "A", "C", "A", "D", "A", "C"]
 
-nota_aluno = 0
+respostas = ["B", "C", "B", "A", "C", "A", "D", "A", "C"] # Lista contendo as respostas do aluno
 
-for i in range(0, len(gabarito)):
-    if respostas[i] == gabarito[i]:
-        nota_aluno+=1
-    
+nota_aluno = 0 
+# Variável para armazenar a nota final do aluno
+
+# Loop para comparar as respostas do aluno com o gabarito
+for i in range(0, len(gabarito)): # Itera de 0 até o tamanho da lista 'gabarito'
+    if respostas[i] == gabarito[i]: # Verifica se a resposta do aluno está correta
+        nota_aluno += 1 # Incrementa a nota do aluno para cada resposta correta
         
+
 print(f"Sua nota na prova é {nota_aluno}!")
-
-    
-
-
